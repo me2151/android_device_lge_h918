bd3eb13bc76cb7c8f33884cef75054fc757d87c6
me2151 <dillonbrosche@gmail.com>
2017-06-14 17:59:14 -0400

    Xospify

--- xosp.mk
+++ xosp.mk
@@ -1,12 +1,13 @@
 $(call inherit-product, device/lge/h918/full_h918.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/configs/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/configs/xosp.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h918
+PRODUCT_NAME := xosp_h918
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v20" \
